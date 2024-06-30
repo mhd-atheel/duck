6d0ab547eb988d1e1b77954bb80e2f2fbdcf31de
mhd-atheel <aathilmazz1234@gmail.com>
2024-06-30 23:28:27 +0530

    create new post with image and without image full authenticate add new packages   flutter_local_notifications: ^17.1.2                    firebase_messaging: ^14.9.4                    firebase_storage: ^11.7.6

--- lib/create_post_screen/create_post_view.dart
+++ lib/create_post_screen/create_post_view.dart
@@ -2,13 +2,14 @@ import 'package:duck/create_post_screen/create_post_controller.dart';
 import 'package:duck/initializeProviders.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../global_colors.dart';
 
 class CreatePostView extends ConsumerWidget {
   const CreatePostView({super.key});
 
-  void _settingModalBottomSheet(context,createPostController) {
+  void settingModalBottomSheet(context,createPostController) {
     showModalBottomSheet(
         context: context,
         builder: (BuildContext bc) {
@@ -35,7 +36,7 @@ class CreatePostView extends ConsumerWidget {
                       GestureDetector(
                         onTap: () async {
                           Navigator.pop(context);
-                          createPostController.imagePickerMethod();
+                          createPostController.imagePickerMethod(ImageSource.camera);
                         },
                         child: const Column(
                           mainAxisAlignment: MainAxisAlignment.center,
@@ -54,7 +55,7 @@ class CreatePostView extends ConsumerWidget {
                       GestureDetector(
                         onTap: () {
                           Navigator.pop(context);
-                          createPostController.imagePickerMethod();
+                          createPostController.imagePickerMethod(ImageSource.gallery);
                         },
                         child: const Column(
                           mainAxisAlignment: MainAxisAlignment.center,
@@ -97,177 +98,196 @@ class CreatePostView extends ConsumerWidget {
         automaticallyImplyLeading: false,
 
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 15.0,top: 8,bottom: 0,right: 15),
-                child: Row(
-                  children: [
-                    Text(
-                      'Whats on your mind?',
-                      style: TextStyle(
-                          fontSize: 18,
-                          color: GlobalColor.blackBold
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 15.0,top: 8,bottom: 0,right: 15),
+                  child: Row(
+                    children: [
+                      Text(
+                        'Whats on your mind?',
+                        style: TextStyle(
+                            fontSize: 18,
+                            color: GlobalColor.blackBold
+                        ),
                       ),
-                    ),
-                    const Text(
-                      ' *',
-                      style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.red
+                      const Text(
+                        ' *',
+                        style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.red
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 15.0,top: 8,bottom: 8,right: 15),
-                  child: Container(
-                    padding:const EdgeInsets.symmetric(horizontal: 20),
-                    height: 100,
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 15.0,top: 8,bottom: 8,right: 15),
+                    child: Container(
+                      padding:const EdgeInsets.symmetric(horizontal: 20),
+                      height: 100,
+                      decoration: BoxDecoration(
+                          color: const Color(0xffF7F8F9),
+                          border: Border.all(
+                            color: createPostController.isThoughtsNull?Colors.red: const Color(0xffE8ECF4),
+                          ),
+                          borderRadius: BorderRadius.circular(15)
+                      ),
+                      child: TextFormField(
+                        maxLines: 4,
+                        controller: createPostController.thoughtsController,
+                        cursorColor: createPostController.isThoughtsNull?Colors.red:Colors.black,
+                        onChanged: (val){
+                          createPostController.clearThoughtsWarning(val);
+                        },
+                        decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'add your thoughts...',
+                            hintStyle: TextStyle(
+                                color: Color(0xff8391A1),
+                                fontSize: 15
+                            ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+        
+              ],
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 15.0,top: 5,bottom: 0,right: 15),
+                  child: Text(
+                    'Your capture?',
+                    style: TextStyle(
+                        fontSize: 18,
+                        color: GlobalColor.blackBold
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            GestureDetector(
+              onTap: () {
+                settingModalBottomSheet(context,createPostController);
+              },
+              child: Padding(
+                padding:
+                const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+                child: Container(
+                    width: MediaQuery.of(context).size.width,
                     decoration: BoxDecoration(
+                      // 0xfff2f2f2  - like a gray
                         color: const Color(0xffF7F8F9),
                         border: Border.all(
                           color: const Color(0xffE8ECF4),
                         ),
-                        borderRadius: BorderRadius.circular(15)
-                    ),
-                    child: TextFormField(
-                      maxLines: 4,
-                      decoration: const InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'add your thoughts...',
-                          hintStyle: TextStyle(
-                              color: Color(0xff8391A1),
-                              fontSize: 15
-                          )
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-
-            ],
-          ),
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 15.0,top: 5,bottom: 0,right: 15),
-                child: Text(
-                  'Your capture?',
-                  style: TextStyle(
-                      fontSize: 18,
-                      color: GlobalColor.blackBold
-                  ),
-                ),
-              ),
-            ],
-          ),
-          GestureDetector(
-            onTap: () {
-              _settingModalBottomSheet(context,createPostController);
-            },
-            child: Padding(
-              padding:
-              const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
-              child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(
-                    // 0xfff2f2f2  - like a gray
-                      color: const Color(0xffF7F8F9),
-                      border: Border.all(
-                        color: const Color(0xffE8ECF4),
-                      ),
-                      borderRadius: BorderRadius.circular(5),
-                      ),
-                  child: createPostController.image == null
-                      ? const SizedBox(
-                    height: 150,
-                    child: Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.image,
-                            size: 50,
-                          ),
-                          Text("Select Image")
-                        ],
-                      ),
-                    ),
-                  )
-                      : Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width,
-                        child: Image.file(createPostController.image!,fit: BoxFit.fill,),
-                      ),
-                      const Center(
+                        borderRadius: BorderRadius.circular(5),
+                        ),
+                    child: createPostController.image == null
+                        ? const SizedBox(
+                      height: 150,
+                      child: Center(
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             Icon(
                               Icons.image,
                               size: 50,
-
-                              color: Colors.white,
                             ),
-                            Text(
-                              "Select Image",
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold),
-                            )
+                            Text("Select Image")
                           ],
                         ),
                       ),
-                      Positioned(
-                          top: 10,
-                          right: 5,
-                          child: GestureDetector(
-                            onTap: () {
-                              createPostController.closeGalleryImage();
-                            },
-                            child: const CircleAvatar(
-                                backgroundColor: Colors.white,
-                                child: Icon(Icons.close)),
-                          ))
-                    ],
-                  )),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 15.0,top: 50,bottom: 0,right: 15),
-            child: Container(
-              height: 50,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                  color:  GlobalColor.mainColor,
-                  borderRadius: BorderRadius.circular(8)
+                    )
+                        : Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          child: Image.file(createPostController.image!,fit: BoxFit.fill),
+                        ),
+                        const Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.image,
+                                size: 50,
+        
+                                color: Colors.white,
+                              ),
+                              Text(
+                                "Select Image",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              )
+                            ],
+                          ),
+                        ),
+                        Positioned(
+                            top: 10,
+                            right: 5,
+                            child: GestureDetector(
+                              onTap: () {
+                                createPostController.closeGalleryImage();
+                              },
+                              child: const CircleAvatar(
+                                  backgroundColor: Colors.white,
+                                  child: Icon(Icons.close)),
+                            ))
+                      ],
+                    )),
               ),
-              child: const Center(
-                child: Text(
-                  'create post',
-                  style: TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.w500,
-                      color:Colors.white
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0,top: 50,bottom: 0,right: 15),
+              child: GestureDetector(
+                onTap: (){
+                  createPostController.createPost(context);
+                },
+                child: Container(
+                  height: 50,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                      color:  GlobalColor.mainColor,
+                      borderRadius: BorderRadius.circular(8)
                   ),
+                  child: Center(
+                    child: createPostController.isCreatePost ==false?
+                    const Text(
+                      "Create post",
+                      style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w500,
+                          color:Colors.white
+                      ),
+                    ):const SizedBox(
+                      height: 30.0,
+                      width: 30.0,
+                      child: CircularProgressIndicator(
+                        color: Colors.white,
+                      ),
+                    ),
+                  )
                 ),
               ),
-            ),
-          )
-
-        ],
+            )
+        
+          ],
+        ),
       ),
     );
   }
